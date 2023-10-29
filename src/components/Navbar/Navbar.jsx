beef2dac70e3a9a25c5e96b7a374ca885de3bed5
Akinfenwa Gabriel <gabrielakinfenwa2019@gmail.com>
2023-10-29 19:26:36 +0100

    Added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -25,7 +24,6 @@ const Navbar = () => {
     </div>
     <div className='app__navbar-smallscreen'>
       <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => { setToggleMenu(true) }} />
-{/*Mobile Screen*/}
       {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
           <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
